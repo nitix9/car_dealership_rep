4a0b58b2fb52c8d47133c0dc0800145d59bf2a45
Nitix 9 <Romashko.04@mail.ru>
2023-10-31 21:44:35 +0500

    Добавлен фон

--- car_dealership/Form1.cs
+++ car_dealership/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace car_dealership
 {
@@ -15,6 +16,14 @@ namespace car_dealership
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = conn.GetConnection();
+            string sql = $"INSERT INTO Types_car (name_types) VALUES ('sedan')";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            int number = cmd.ExecuteNonQuery();
         }
     }
 }
