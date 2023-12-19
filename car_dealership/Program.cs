8927c3047fa0328ed24e2d1ddfb47d1b1d8ce01c
nitix9 <148373340+nitix9@users.noreply.github.com>
2023-12-19 19:49:26 +0500

    функционал формы отправки заявок

--- car_dealership/Program.cs
+++ car_dealership/Program.cs
@@ -16,7 +16,7 @@ namespace car_dealership
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new admin());
+            Application.Run(new user());
         }
     }
 }
--- car_dealership/zayavki.cs
+++ car_dealership/zayavki.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace car_dealership
 {
@@ -15,6 +16,31 @@ namespace car_dealership
         public zayavki()
         {
             InitializeComponent();
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            string sqlz = "INSERT INTO applications SELECT NULL," + carstore.dt.Rows[carstore.index][0].ToString() + ",id FROM users WHERE mobile_phone = @number and users.name = @name and users.last_name = @last_name and users.patronymic = @patronymic";
+            MySqlConnection con = conn.GetConnection();
+            MySqlCommand cmdt = new MySqlCommand(sqlz, con);
+            cmdt.Parameters.Add("@number", MySqlDbType.VarChar).Value = numbertxt.Text;
+            cmdt.Parameters.Add("@name", MySqlDbType.VarChar).Value = nametxt.Text;
+            cmdt.Parameters.Add("@last_name", MySqlDbType.VarChar).Value = lastntxtbx.Text;
+            cmdt.Parameters.Add("@patronymic", MySqlDbType.VarChar).Value = otchtxt.Text;
+            try
+            {
+                int n=cmdt.ExecuteNonQuery();
+                if (n != 0)
+                {
+                    MessageBox.Show("Заявка отправлена!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                        MessageBox.Show("Неверные данные, заявка не отправлена!","Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch { }
+            con.Close();
         }
     }
 }
